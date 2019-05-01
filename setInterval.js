22e0ee39904c4e746b124c1c32b699fbbe871714
Jacob Benaim <benaimjacob@gmail.com>
2019-05-01 03:57:07 +0000

    added setTimeout.js and setInterval.js

---
+++ setInterval.js
@@ -0,0 +1,4 @@
+function sayHello() {
+    alert("Hello");
+  }
+  var timer = setInterval(sayHello, 3000);
---
+++ setTimeout.js
@@ -0,0 +1,8 @@
+var helloWorld = "Hello World"; 
+
+function sayHello() {
+  alert("Hello");
+}
+setTimeout(sayHello, 3000);
+
+// timer will not end ... refer to setInterval.js
